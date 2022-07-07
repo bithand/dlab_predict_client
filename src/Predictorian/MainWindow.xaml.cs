06ace14be4df80a24f73fb1c63b325ca7fd29518
tomcat <thomas.arndt@bithand.de>
2022-07-07 22:13:35 +0200

    + add scaffold

---
+++ src/Predictorian/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Flurl.Http;
+using Predictorian.data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Predictorian
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                string address = "http://office.bithand.de.com:8485/predict";
+                string file = "image_1.jpg";
+                Task<IFlurlResponse> t = DataHandler.GetMultipartData(address, file);
+                t.Wait();
+                Trace.WriteLine("call done");
+                Trace.WriteLine(t.Result.ResponseMessage.ToString());
+                if (t.Result.StatusCode == 200)
+                {
+                    Task<byte[]> b = t.Result.GetBytesAsync();
+                    b.Wait();
+                    Trace.WriteLine(b.Result.Length);
+                    File.WriteAllBytes("out.jpg", b.Result);
+                }
+            });
+        }
+    }
+}
---
+++ src/Predictorian/data/DataHandler.cs
@@ -0,0 +1,20 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predictorian.data
+{
+    public class DataHandler
+    {
+        public static async Task<IFlurlResponse> GetMultipartData(string address,string imagefile)
+        {
+            var resp = await address.WithHeaders(new {User_Agent="Predictorian"}).PostMultipartAsync(mp => mp
+                 .AddFile("file", imagefile));
+            return resp;
+        }
+
+    }
+}
